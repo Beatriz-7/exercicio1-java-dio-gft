9d91596ee47b81ea79de1d7b6b0ba3e7c54fa4ce
Beatriz-7 <beatrizlcirino@gmail.com>
2022-05-12 13:20:23 -0300

    Exercícios Java

---
+++ src/one/digitalinnovation/bootcamp/dio/Main.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.bootcamp.dio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,3,2);
+        Quadrilatero.area(5f,5f);
+    }
+
+}
